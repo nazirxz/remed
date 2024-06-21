aea0e17245f2f50e051b8c05cd64650a516048cf
nazirxz <nzryanzr@gmail.com>
2024-06-21 15:29:57 +0700

    adding splash screen

---
+++ app/src/main/java/com/example/remed/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.example.remed
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+
+        // Add a delay of 3 seconds (3000 milliseconds)
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Start the main activity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            // Finish the splash screen activity so the user cannot return to it
+            finish()
+        }, 3000)
+    }
+}
