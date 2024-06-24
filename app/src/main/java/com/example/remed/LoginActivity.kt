960b93fe76b84f791477c4c0151f5c11b6733a1f
nazirxz <nzryanzr@gmail.com>
2024-06-24 08:22:50 +0700

    change name of the file

--- app/src/main/java/com/example/remed/LoginActivity.kt
+++ app/src/main/java/com/example/remed/LoginActivity.kt
@@ -3,9 +3,9 @@ package com.example.remed
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class RegisterActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.register_activity)
+        setContentView(R.layout.login_activity)
     }
 }
