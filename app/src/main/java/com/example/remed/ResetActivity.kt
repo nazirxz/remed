615a37f910038aabcbffd701d125f1789de7c5a7
nazirxz <nzryanzr@gmail.com>
2024-06-26 11:42:40 +0700

    sudah bisa reset password berdasarkan email

--- app/src/main/java/com/example/remed/ResetActivity.kt
+++ app/src/main/java/com/example/remed/ResetActivity.kt
@@ -1,14 +1,45 @@
 package com.example.remed
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.remed.databinding.ForgetpassActivityBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidUserException
 
 class ResetActivity : AppCompatActivity() {
     private lateinit var binding: ForgetpassActivityBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ForgetpassActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        auth = FirebaseAuth.getInstance()
+
+        binding.buttonReset.setOnClickListener {
+            val email = binding.resetPassEmail.text.toString()
+
+            if (email.isEmpty()) {
+                Toast.makeText(this, "Please enter your email", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            auth.sendPasswordResetEmail(email)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "Password reset email sent to $email", Toast.LENGTH_SHORT).show()
+                    } else {
+                        val exception = task.exception
+                        if (exception is FirebaseAuthInvalidUserException) {
+                            Toast.makeText(this, "Account not registered", Toast.LENGTH_SHORT).show()
+                        } else {
+                            Toast.makeText(this, "Failed to send password reset email", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+        }
+
+
     }
 }
