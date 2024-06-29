2f70ad2bfc44338448cae016662a1c5d208f187d
nazirxz <nzryanzr@gmail.com>
2024-06-29 20:18:49 +0700

    adding keranjang detail obat layout

--- app/src/main/java/com/example/remed/KeranjangDetailActivity.kt
+++ app/src/main/java/com/example/remed/KeranjangDetailActivity.kt
@@ -6,21 +6,18 @@ import android.util.Log
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
-import com.example.remed.databinding.KeranjangActivityBinding // Pastikan ini benar
+import com.example.remed.databinding.KeranjangDetailActivityBinding
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
-class KeranjangActivity : AppCompatActivity() {
-
-    private lateinit var binding: KeranjangActivityBinding
-
+class KeranjangDetailActivity:AppCompatActivity() {
+    private lateinit var binding: KeranjangDetailActivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = KeranjangActivityBinding.inflate(layoutInflater)
+        binding= KeranjangDetailActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         // 1. Dapatkan userId (sesuaikan dengan cara Anda menyimpan/mendapatkan data pengguna)
         // Contoh: jika Anda mendapatkannya dari SharedPreferences
         val userId = intent.getStringExtra("userId")
@@ -44,13 +41,7 @@ class KeranjangActivity : AppCompatActivity() {
             finish()
             intent.putExtra("userId", userId)
         }
-        binding.btnNext.setOnClickListener {
-            val intent = Intent(this,KeranjangDetailActivity::class.java)
-            intent.putExtra("userId",userId)
-            startActivity(intent)
-        }
     }
-
     fun setProfileImageFromFirebase(imageView: ImageView, userId: String) {
         val database = FirebaseDatabase.getInstance()
         val userRef = database.getReference("users/$userId")
